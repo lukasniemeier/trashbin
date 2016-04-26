7f66b2ccfba108f6431f3dad7dfe08bd69b9e790
Lukas Niemeier <lukas.niemeier@zalando.de>
2016-04-26 21:04:55 +0200

    Allow any json subformat to be parsed as json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var RingBuffer = require('./ring.js');
 var bodyParser = require('body-parser');
-var multer = require('multer'); 
+var multer = require('multer');
 
 var app = express();
 
@@ -9,18 +9,21 @@ var ring = new RingBuffer(parseInt(process.env.CONFIG_BUFFER_SIZE || 5));
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser.json({strict: false}));
+app.use(bodyParser.json({strict: false, type: function(req) {
+	return req.is('json') || req.is('application/*+json');
+}}));
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
 
 app.all('*', function(request, response) {
-	var body = { 
+	var body = {
 		"time": new Date().toISOString(),
-		"source": request.ip, 
+		"source": request.ip,
 		"method": request.method,
-		"path": request.url, 
-		"headers": request.rawHeaders, 
+		"path": request.url,
+		"headers": request.headers,
+		"rawHeaders": request.rawHeaders,
 		"body": (request.body || "null")
 	};
 	ring.enq(body);
